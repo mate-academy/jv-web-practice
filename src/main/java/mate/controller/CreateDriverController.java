2e17b07d2cddca4f7c1690f6ab4c772f951a528b
m.kislukhina <marharita.kislukhina@gmail.com>
2021-05-25 11:02:30 +0200

    fixed

--- src/main/java/mate/controller/CreateDriverController.java
+++ src/main/java/mate/controller/CreateDriverController.java
@@ -7,15 +7,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.lib.Injector;
-import mate.service.CarService;
+import mate.model.Driver;
 import mate.service.DriverService;
 
 @WebServlet(urlPatterns = "/addDriver")
 public class CreateDriverController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
-    private final CarService carService = (CarService) injector.getInstance(CarService.class);
-    private final DriverService driverService = (DriverService) injector
-            .getInstance(DriverService.class);
+    private final DriverService driverService
+            = (DriverService) injector.getInstance(DriverService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
@@ -24,11 +23,10 @@ public class CreateDriverController extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-        Long carId = Long.valueOf(req.getParameter("car_id"));
-        Long driverId = Long.valueOf(req.getParameter("driver_id"));
-        carService.addDriverToCar(driverService.get(driverId), carService.get(carId));
-        resp.sendRedirect("/cars/");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        String name = req.getParameter("name");
+        String licenseNumber = req.getParameter("number");
+        Driver driver = new Driver(name, licenseNumber);
+        driverService.create(driver);
     }
 }
