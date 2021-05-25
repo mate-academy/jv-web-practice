488a6f57867a5ac376317a6512db3aa20c45a461
MakeDreamCode <aklimenchuk@gmail.com>
2021-05-25 14:48:50 +0300

    Removed Manufacturer service.

--- src/main/java/mate/controller/GetAllCarsController.java
+++ src/main/java/mate/controller/GetAllCarsController.java
@@ -8,15 +8,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.lib.Injector;
 import mate.service.CarService;
-import mate.service.ManufacturerService;
 
 @WebServlet(urlPatterns = "/cars")
 public class GetAllCarsController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
     private final CarService carService =
             (CarService) injector.getInstance(CarService.class);
-    private final ManufacturerService manufacturerService =
-            (ManufacturerService) injector.getInstance(ManufacturerService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
