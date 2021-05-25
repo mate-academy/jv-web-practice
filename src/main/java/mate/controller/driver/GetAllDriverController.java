9c00ba66dc3064aa644080f88707a887bbadb899
redxp <redxpertus1@gmail.com>
2021-05-25 09:54:55 +0300

    WebPractice hw v02 made work with Igor comments

--- src/main/java/mate/controller/driver/GetAllDriverController.java
+++ src/main/java/mate/controller/driver/GetAllDriverController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import mate.lib.Injector;
 import mate.service.DriverService;
 
-@WebServlet("/drivers/all")
+@WebServlet(urlPatterns = "/drivers/all")
 public class GetAllDriverController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
     private final DriverService driverService = (DriverService) injector
