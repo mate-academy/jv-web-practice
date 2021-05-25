061ceb713eba2f91e916cfe1a5b43ba125480bf3
Halina Kharlanava <galina.harlanova1@gmail.com>
2021-05-25 10:58:07 +0200

    Implemented suggested changes.

--- src/main/java/mate/controller/CreateDriverController.java
+++ src/main/java/mate/controller/CreateDriverController.java
@@ -19,7 +19,7 @@ public class CreateDriverController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/drivers/create.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/driver/create.jsp").forward(req, resp);
     }
 
     @Override
@@ -28,5 +28,4 @@ public class CreateDriverController extends HttpServlet {
         driverService.create(new Driver(req.getParameter("name"),
                 req.getParameter("licenseNumber")));
     }
-
 }
--- src/main/java/mate/controller/GetAllDriversController.java
+++ src/main/java/mate/controller/GetAllDriversController.java
@@ -13,7 +13,7 @@ import mate.service.DriverService;
 
 @WebServlet(urlPatterns = "/drivers/all")
 public class GetAllDriversController extends HttpServlet {
-    private final Injector injector = Injector.getInstance("mate");
+    private static final Injector injector = Injector.getInstance("mate");
     private final DriverService driverService =
             (DriverService) injector.getInstance(DriverService.class);
 
@@ -22,6 +22,6 @@ public class GetAllDriversController extends HttpServlet {
             throws ServletException, IOException {
         List<Driver> allDrivers = driverService.getAll();
         req.setAttribute("drivers", allDrivers);
-        req.getRequestDispatcher("/WEB-INF/views/drivers/all.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/driver/all.jsp").forward(req, resp);
     }
 }
