43566c9363acfcbbb0ffd51c0fb854bed8dec66c
mbelm <mbelmasg@gmail.com>
2021-06-30 20:51:58 +0300

    was added CreateDriverController, and created first driver in DB, me

---
+++ src/main/java/mate/controller/CreateDriverController.java
@@ -0,0 +1,32 @@
+package mate.controller;
+
+import mate.lib.Injector;
+import mate.model.Driver;
+import mate.service.DriverService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CreateDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate"); // be careful with copy-paste
+    private final DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("WEB-INF/views/create_driver.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String licenseNumber = request.getParameter("license_number");
+        Driver newDriver = new Driver(name, licenseNumber);
+        driverService.create(newDriver);
+
+    }
+}
