489a5453950968c1fe1932386e6ac2b0a49eab68
Denis <deniskichan@gmail.com>
2021-07-21 13:30:21 +0300

    fixed spelling mistake

--- src/main/java/mate/controller/GetAllDriversController.java
+++ src/main/java/mate/controller/GetAllDriversController.java
@@ -12,7 +12,7 @@ import mate.model.Driver;
 import mate.service.DriverService;
 
 @WebServlet(urlPatterns = "/drivers")
-public class GetAlDriversController extends HttpServlet {
+public class GetAllDriversController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
     private DriverService driverService;
 
