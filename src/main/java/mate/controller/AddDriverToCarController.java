6104448158c5af04aaea190497a8ada45db71b1d
docer <vlasovengineer@gmail.com>
2021-07-22 12:44:54 +0300

    fix checkstyle

--- src/main/java/mate/controller/AddDriverToCarController.java
+++ src/main/java/mate/controller/AddDriverToCarController.java
@@ -1,7 +1,6 @@
 package mate.controller;
 
 import java.io.IOException;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
