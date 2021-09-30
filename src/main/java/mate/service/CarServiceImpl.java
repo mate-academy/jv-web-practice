4447fe1fa906256c7106a9bc339e86de4df2f322
Sinelife <ya.serzhan@gmail.com>
2021-09-30 16:20:29 +0300

    fixed checkstyles

--- src/main/java/mate/service/CarServiceImpl.java
+++ src/main/java/mate/service/CarServiceImpl.java
@@ -1,9 +1,7 @@
 package mate.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import mate.dao.CarDao;
 import mate.dao.DriverDao;
 import mate.lib.Inject;
