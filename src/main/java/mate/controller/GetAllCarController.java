e074048b3c98542b7057985db184376864b4b069
Roman Kozhevnikow <roman.kozhevnikow@gmail.com>
2021-10-11 21:41:07 +0300

    add all needed servlets and jsp pages.

---
+++ src/main/java/mate/controller/GetAllCarController.java
@@ -0,0 +1,35 @@
+package mate.controller;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.model.Car;
+import mate.service.CarService;
+
+@WebServlet(urlPatterns = "/cars")
+public class GetAllCarController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final CarService carService = (CarService)
+            injector.getInstance(CarService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        List<Car> cars = carService.getAll();
+        req.setAttribute("allCars", cars);
+        req.getRequestDispatcher("/WEB-INF/car/getAllCars.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String carId = req.getParameter("car_id");
+        carService.delete(Long.valueOf(carId));
+        resp.sendRedirect(req.getContextPath() + "/cars");
+    }
+}
