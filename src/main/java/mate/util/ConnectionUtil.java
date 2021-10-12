d064807d19fd4cff0c872d5e391fc4d4bced069e
Vladyslav Gural <vladgural@gmail.com>
2021-10-12 17:53:38 +0300

    remove a few constants

--- src/main/java/mate/util/ConnectionUtil.java
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -8,10 +8,8 @@ import java.util.Properties;
 public class ConnectionUtil {
     public static final String DRIVER_NAME = "org.mariadb.jdbc.Driver";
     public static final String URL = "jdbc:mariadb://192.168.1.40:3306/taxi_service_db";
-    public static final String USER = "user";
-    public static final String USER_NAME = "root";
-    public static final String PASSWORD = "password";
-    public static final String PASSWORD_NAME = "MariaDB#10";
+    public static final String USER = "root";
+    public static final String PASSWORD = "MariaDB#10";
 
     static {
         try {
@@ -24,8 +22,8 @@ public class ConnectionUtil {
     public static Connection getConnect() {
         try {
             Properties dbProperties = new Properties();
-            dbProperties.put(USER, USER_NAME);
-            dbProperties.put(PASSWORD, PASSWORD_NAME);
+            dbProperties.put("user", USER);
+            dbProperties.put("password", PASSWORD);
             return DriverManager.getConnection(URL, dbProperties);
         } catch (SQLException e) {
             throw new RuntimeException("Can't create connection to DB", e);
