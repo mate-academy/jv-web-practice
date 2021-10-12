401fc36d992d12fbe9d1841fdfb6f383c5785bd4
Vladyslav Gural <vladgural@gmail.com>
2021-10-12 17:39:03 +0300

    replace long to Long

--- src/main/java/mate/controller/car/AddNewDriverToCarController.java
+++ src/main/java/mate/controller/car/AddNewDriverToCarController.java
@@ -28,8 +28,8 @@ public class AddNewDriverToCarController extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        long carId = Long.parseLong(req.getParameter("car_id"));
-        long driverId = Long.parseLong(req.getParameter("driver_id"));
+        Long carId = Long.valueOf(req.getParameter("car_id"));
+        Long driverId = Long.valueOf(req.getParameter("driver_id"));
         Car car = carService.get(carId);
         Driver driver = driverService.get(driverId);
         carService.addDriverToCar(driver, car);
