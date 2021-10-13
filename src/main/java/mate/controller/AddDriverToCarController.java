437f97c5ef840e5622a32ba88c05d96cfcc8da69
Roman Legendzevych <green3942@gmail.com>
2021-10-13 17:44:18 +0300

    add some fix

--- src/main/java/mate/controller/AddDriverToCarController.java
+++ src/main/java/mate/controller/AddDriverToCarController.java
@@ -1,7 +1,6 @@
 package mate.controller;
 
 import java.io.IOException;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,10 +22,6 @@ public class AddDriverToCarController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        List<Car> cars = carService.getAll();
-        List<Driver> drivers = driverService.getAll();
-        req.setAttribute("cars", cars);
-        req.setAttribute("drivers", drivers);
         req.getRequestDispatcher("/WEB-INF/views/drivers/addDriverToCar.jsp").forward(req, resp);
     }
 
