4052434593b22018166dfbcf621f967fb1ca4704
Vladyslav Chyver <lione2009lione@gmail.com>
2021-12-12 18:33:31 +0200

    mvn clean package

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -8,7 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 
 public class IndexController extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/views/index.jsp")
                 .forward(request, response);
     }
