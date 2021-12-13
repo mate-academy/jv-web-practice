22c1c96efede84b118ad0d3bb01d352cfa7c30cf
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-12-13 16:26:45 +0200

    empty line was removed

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/")
 public class IndexController extends HttpServlet {
-
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
