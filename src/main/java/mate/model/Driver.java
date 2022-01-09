0a74541f8e4db1264e543fe8c8664708abe1c8c8
ASelivonchyk <aselivonchyk@gmail.com>
2022-01-09 18:23:42 +0200

    changed constructors in Model packge.

--- src/main/java/mate/model/Driver.java
+++ src/main/java/mate/model/Driver.java
@@ -7,6 +7,10 @@ public class Driver {
     private String name;
     private String licenseNumber;
 
+    public Driver() {
+
+    }
+
     public Driver(String name, String licenseNumber) {
         this.name = name;
         this.licenseNumber = licenseNumber;
