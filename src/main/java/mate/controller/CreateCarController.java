bafb786bc9c4fcd9cab6dc6cd85ea5b85545a643
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-02-06 23:19:21 +0200

    Fix carCreateForm.jsp

--- src/main/java/mate/controller/CreateCarController.java
+++ src/main/java/mate/controller/CreateCarController.java
@@ -1,16 +1,36 @@
 package mate.controller;
 
+import mate.lib.Injector;
+import mate.model.Manufacturer;
+import mate.service.CarService;
+import mate.service.ManufacturerService;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class CreateCarController extends HttpServlet {
     private static final String CAR_CREATE_FORM = "/WEB-INF/views/carCreateForm.jsp";
+    private static final String ROOT_PACKAGE = "mate";
+    private static final String MANUFACTURERS_ATTRIBUTE = "manufacturers";
+    private CarService carService;
+    private ManufacturerService manufacturerService;
+
+    @Override
+    public void init() {
+        Injector injector = Injector.getInstance(ROOT_PACKAGE);
+        carService = (CarService) injector
+                .getInstance(CarService.class);
+        manufacturerService = (ManufacturerService) injector
+                .getInstance(ManufacturerService.class);
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Manufacturer> manufacturers = manufacturerService.getAll();
+        req.setAttribute(MANUFACTURERS_ATTRIBUTE, manufacturers);
         req.getRequestDispatcher(CAR_CREATE_FORM).forward(req, resp);
     }
 
--- src/main/webapp/WEB-INF/views/carCreateForm.jsp
+++ src/main/webapp/WEB-INF/views/carCreateForm.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -11,8 +12,25 @@
 <body>
 <h1>Create new car from follow data</h1>
 <form method="post" action="${pageContext.request.contextPath}/cars/add">
-    Model <input type="text" name="model"><br>
-    Manufacturer <input type="text" name="manufacturer"><br>
+    Model: <input type="text" name="model"><br>
+    <label>Select manufacturer</label> <br>
+    <table>
+        <tr>
+            <td>SELECT</td>
+            <td>ID</td>
+            <td>NAME</td>
+            <td>COUNTRY</td>
+        </tr>
+        <c:forEach items="${manufacturers}" var="manufacturer">
+        <tr>
+            <td><input type="radio" name="manufacturer_id" value="${manufacturer.getId()}"></td>
+            <td><c:out value="${manufacturer.getId()}" /></td>
+            <td><c:out value="${manufacturer.getName()}" /></td>
+            <td><c:out value="${manufacturer.getCountry()}" /></td>
+        </tr>
+        </c:forEach>
+    </table>
+    <br><br>
     <button type="submit" onclick="fun()">Create</button>
 </form>
 </body>
