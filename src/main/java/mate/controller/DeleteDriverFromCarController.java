00207f52e97b5638e28592b91736ab6c4fe62565
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-02-07 20:41:44 +0200

    Add DeleteDriverFromCarController

---
+++ src/main/java/mate/controller/DeleteDriverFromCarController.java
@@ -0,0 +1,44 @@
+package mate.controller;
+
+import mate.lib.Injector;
+import mate.model.Car;
+import mate.model.Driver;
+import mate.service.CarService;
+import mate.service.DriverService;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class DeleteDriverFromCarController extends HttpServlet {
+    private static final String ROOT_PACKAGE = "mate";
+    private static final String DRIVERS_BY_CAR_REQUEST = "/cars/drivers";
+    private static final String DRIVER_ID_ATTRIBUTE = "driver_id";
+    private static final String CAR_ID_ATTRIBUTE = "car_id";
+    private DriverService driverService;
+    private CarService carService;
+
+    @Override
+    public void init() throws ServletException {
+        Injector injector = Injector.getInstance(ROOT_PACKAGE);
+        driverService = (DriverService) injector
+                .getInstance(DriverService.class);
+        carService = (CarService) injector
+                .getInstance(CarService.class);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        long driverId = Long.parseLong(req.getParameter(DRIVER_ID_ATTRIBUTE));
+        long carId = Long.parseLong(req.getParameter(CAR_ID_ATTRIBUTE));
+        Car car = carService.get(carId);
+        Driver driver = driverService.get(driverId);
+        carService.removeDriverFromCar(driver, car);
+        resp.sendRedirect(getServletContext().getContextPath()
+                + DRIVERS_BY_CAR_REQUEST
+                + "?" + CAR_ID_ATTRIBUTE + "=" + carId);
+    }
+}
