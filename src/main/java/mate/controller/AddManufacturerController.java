2f02bb400d37f63aff74e670ed6e4cda23e93f43
LinkClink <shekaqwe@gmail.com>
2022-03-09 18:42:46 +0200

    mvn fix

--- src/main/java/mate/controller/AddManufacturerController.java
+++ src/main/java/mate/controller/AddManufacturerController.java
@@ -17,7 +17,8 @@ public class AddManufacturerController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/manufacturers/addManufacturer.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/manufacturers/addManufacturer.jsp")
+                .forward(req, resp);
     }
 
     @Override
