e18dbc007a810e2efef2173ecfc39267f4444486
Sasha Bachynskyi <allfred12@gmail.com>
2022-03-10 13:26:13 +0200

    implemented CreateNewManufacturerController and its jsp page

--- src/main/java/mate/controller/CreateNewManufacturerController.java
+++ src/main/java/mate/controller/CreateNewManufacturerController.java
@@ -1,8 +1,8 @@
 package mate.controller;
 
 import mate.lib.Injector;
-import mate.model.Driver;
-import mate.service.DriverService;
+import mate.model.Manufacturer;
+import mate.service.ManufacturerService;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,25 +10,25 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/drivers/add")
-public class CreateNewDriverController extends HttpServlet {
+@WebServlet(urlPatterns = "/manufacturer/add")
+public class CreateNewManufacturerController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
-    private final DriverService driverService =
-            (DriverService) injector.getInstance(DriverService.class);
+    private final ManufacturerService manufacturerService =
+            (ManufacturerService) injector.getInstance(ManufacturerService.class);
 
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/newDriver.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/newManufacturer.jsp").forward(req, resp);
     }
 
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        Driver driver = new Driver();
-        driver.setName(req.getParameter("name"));
-        driver.setLicenseNumber(req.getParameter("licenseDriver"));
-        driverService.create(driver);
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setName(req.getParameter("name"));
+        manufacturer.setCountry(req.getParameter("country"));
+        manufacturerService.create(manufacturer);
         resp.sendRedirect("/index");
     }
 }
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -4,9 +4,10 @@
     <title>Taxi service</title>
 </head>
 <body>
-<h1>Welcome to taxi service</h1>
-<p>Please, choose your choice below: </p>
-<a href="${pageContext.request.contextPath}/drivers/add">Create a new driver</a><br>
-<a href="${pageContext.request.contextPath}/drivers/all">Show all drivers</a>
+    <h1>Welcome to taxi service</h1>
+    <p>Please, choose your choice below: </p>
+    <a href="${pageContext.request.contextPath}/drivers/add">Create a new driver</a><br>
+    <a href="${pageContext.request.contextPath}/drivers/all">Show all drivers</a><br>
+    <a href="${pageContext.request.contextPath}/manufacturer/add">Create a new manufacturer</a>
 </body>
 </html>
