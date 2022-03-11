7fb3b829dcf2934fab10daf978d14dec4f31c9af
Liliia Ponomarenko <liliya97ua@gmail.com>
2022-03-11 08:23:08 +0200

    implemented CreateCarController

--- src/main/java/mate/controller/CreateCarController.java
+++ src/main/java/mate/controller/CreateCarController.java
@@ -1,32 +1,38 @@
 package mate.controller;
 
-import java.io.IOException;
+import mate.lib.Injector;
+import mate.model.Car;
+import mate.model.Manufacturer;
+import mate.service.CarService;
+import mate.service.ManufacturerService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import mate.lib.Injector;
-import mate.model.Manufacturer;
-import mate.service.ManufacturerService;
+import java.io.IOException;
 
-@WebServlet(urlPatterns = "/manufacturers/createManufacturer")
-public class CreateManufacturerController extends HttpServlet {
+@WebServlet(urlPatterns = "/cars/createCar")
+public class CreateCarController extends HttpServlet {
     private final Injector injector = Injector.getInstance("mate");
+    private final CarService carService = (CarService)
+            injector.getInstance(CarService.class);
     private final ManufacturerService manufacturerService = (ManufacturerService)
             injector.getInstance(ManufacturerService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/manufacturers/createManufacturer.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/cars/createCar.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Manufacturer manufacturer = new Manufacturer();
-        manufacturer.setName(req.getParameter("manufacturer_name"));
-        manufacturer.setCountry(req.getParameter("country"));
-        manufacturerService.create(manufacturer);
+        Car car = new Car();
+        car.setModel(req.getParameter("model"));
+        car.setManufacturer(manufacturerService
+                .get(Long.parseLong(req.getParameter("manufacturer_id"))));
+        carService.create(car);
         resp.sendRedirect(req.getContextPath() + "/index");
     }
 }
--- src/main/webapp/WEB-INF/views/manufacturers/createManufacturer.jsp
+++ src/main/webapp/WEB-INF/views/manufacturers/createManufacturer.jsp
@@ -5,11 +5,11 @@
 </head>
 <body>
 <h1>Please fill in all clear fields!</h1>
-
 <form method="post" action="${pageContext.request.contextPath}/manufacturers/createManufacturer">
     Name <input type="text" name="manufacturer_name"><br>
     Country <input type="text" name="country"><br>
     <button type="submit">Create</button>
 </form>
+
 </body>
 </html>
