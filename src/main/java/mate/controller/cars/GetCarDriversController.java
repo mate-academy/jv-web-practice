df70ee6bf6364f00bc67468edf093ed2215b8343
Daniil Opryshko <dvo8899@gmail.com>
2022-03-21 13:38:45 +0200

    changed controller naming according to checkstyle

--- src/main/java/mate/controller/cars/GetCarDriversController.java
+++ src/main/java/mate/controller/cars/GetCarDriversController.java
@@ -14,7 +14,7 @@ import mate.service.CarService;
 import mate.service.DriverService;
 
 @WebServlet(urlPatterns = "/cars/drivers")
-public class CarDriversController extends HttpServlet {
+public class GetCarDriversController extends HttpServlet {
     private static final Injector injector
             = Injector.getInstance("mate");
     private static final CarService carService
