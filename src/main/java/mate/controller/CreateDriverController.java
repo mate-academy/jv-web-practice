242993deb973c407d54a5380078991eab14b9714
Olexsii_Titov <drfrankinsheyn16@gmail.com>
2022-03-21 19:16:50 +0200

    CreateDriverController was implemented

--- src/main/java/mate/controller/CreateDriverController.java
+++ src/main/java/mate/controller/CreateDriverController.java
@@ -1,16 +1,36 @@
 package mate.controller;
 
+import mate.lib.Injector;
+import mate.model.Driver;
+import mate.service.DriverService;
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
 
 @WebServlet(urlPatterns = "/drivers/create")
 public class CreateDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/views/drivers/create/create_driver.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String driverName = req.getParameter("driverName");
+        String licenseNumber = req.getParameter("licenseNumber");
+        Driver driver = new Driver();
+        driver.setName(driverName);
+        driver.setLicenseNumber(licenseNumber);
+        driver = driverService.create(driver);
+        System.out.println(driver);
+        req.getRequestDispatcher("/WEB-INF/views/drivers/all_drivers.jsp").forward(req, resp);
     }
 }
