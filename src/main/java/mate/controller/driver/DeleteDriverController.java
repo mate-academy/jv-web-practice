01beaaed6adeb19036d796458ee6a15cd6ee47f5
Oleh Mytiai <mytiai.ov@icloud.com>
2022-03-29 18:39:46 +0300

    implemented DeleteDriverController

--- src/main/java/mate/controller/driver/DeleteDriverController.java
+++ src/main/java/mate/controller/driver/DeleteDriverController.java
@@ -1,7 +1,6 @@
 package mate.controller.driver;
 
 import mate.lib.Injector;
-import mate.model.Driver;
 import mate.service.DriverService;
 
 import javax.servlet.ServletException;
@@ -10,19 +9,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-@WebServlet(urlPatterns = "/drivers")
-public class GetAllDriversController extends HttpServlet {
+@WebServlet(urlPatterns = "/drivers/delete")
+public class DeleteDriverController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
-    private final DriverService driverService =
+    DriverService driverService =
             (DriverService) injector.getInstance(DriverService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        List<Driver> drivers = driverService.getAll();
-        req.setAttribute("drivers", drivers);
-        req.getRequestDispatcher("/WEB-INF/views/drivers/all.jsp").forward(req, resp);
+        Long id = Long.valueOf(req.getParameter("id"));
+        driverService.delete(id);
+        resp.sendRedirect(req.getContextPath() + "/drivers");
     }
 }
--- src/main/webapp/WEB-INF/views/drivers/all.jsp
+++ src/main/webapp/WEB-INF/views/drivers/all.jsp
@@ -10,12 +10,18 @@
         <td>ID</td>
         <td>NAME</td>
         <td>LICENSE NUMBER</td>
+        <td>DELETE</td>
     </tr>
     <c:forEach items="${drivers}" var="driver">
         <tr>
             <td><c:out value="${driver.id}" /></td>
             <td><c:out value="${driver.name}" /></td>
             <td><c:out value="${driver.licenseNumber}" /></td>
+            <td>
+                <a href="${pageContext.request.contextPath}/drivers/delete?id=${driver.id}">
+                    Delete this driver
+                </a>
+            </td>
         </tr>
     </c:forEach>
 </table>
