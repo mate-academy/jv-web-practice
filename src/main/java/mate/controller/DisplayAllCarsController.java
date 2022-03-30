878f4f1ed0b649091a19f7af6526c486b170ebc9
Nazarii Yatsiuk <nazariiyatsiuk@gmail.com>
2022-03-30 11:41:56 +0300

    RemщÐmoved unused import

--- src/main/java/mate/controller/DisplayAllCarsController.java
+++ src/main/java/mate/controller/DisplayAllCarsController.java
@@ -2,7 +2,6 @@ package mate.controller;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.stream.Collectors;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
