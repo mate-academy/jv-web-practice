b8923211e7e294d528a4d2a63ac5524d9d3a0750
IgorZima90 <zimyshkaa@gmail.com>
2022-04-01 02:50:48 +0300

    was implemented controller layer

--- src/main/java/mate/controller/GetAllDriverController.java
+++ src/main/java/mate/controller/GetAllDriverController.java
@@ -7,14 +7,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.lib.Injector;
+import mate.service.DriverService;
 
-@WebServlet(urlPatterns = "/views/index")
-public class IndexController extends HttpServlet {
+@WebServlet(urlPatterns = "/drivers/all")
+public class GetAllDriverController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService
+            = (DriverService) injector.getInstance(DriverService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+        req.setAttribute("drivers", driverService.getAll());
+        req.getRequestDispatcher("/WEB-INF/views/drivers/all.jsp").forward(req, resp);
     }
 }
