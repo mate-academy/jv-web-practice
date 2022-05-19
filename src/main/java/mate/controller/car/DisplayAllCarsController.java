593f18f8b3b60d835c61c8487a321261cb0b4e83
Andrii Synelnyk <sinelnik2154@gmail.com>
2022-05-20 00:59:58 +0300

    Created Controller classes, added jstl dependency

---
+++ src/main/java/mate/controller/car/DisplayAllCarsController.java
@@ -0,0 +1,24 @@
+package mate.controller.car;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.model.Car;
+import mate.service.CarService;
+
+public class DisplayAllCarsController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final CarService carService = (CarService) injector.getInstance(CarService.class);
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List<Car> cars = carService.getAll();
+        request.setAttribute("cars", cars);
+        request.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(request, response);
+    }
+}
---
+++ src/main/java/mate/controller/driver/CreateDriverController.java
@@ -0,0 +1,33 @@
+package mate.controller.driver;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.model.Driver;
+import mate.service.DriverService;
+
+public class CreateDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService
+            = (DriverService) injector.getInstance(DriverService.class);
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        Driver driver = new Driver();
+        driver.setName(request.getParameter("name"));
+        driver.setLicenseNumber(request.getParameter("license_number"));
+        driverService.create(driver);
+        response.sendRedirect(request.getContextPath() + "/drivers");
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/drivers/create.jsp")
+                .forward(request, response);
+    }
+}
