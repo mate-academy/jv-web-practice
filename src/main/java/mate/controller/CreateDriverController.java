af809454c58ae465116bb896577ea65c5a2f3b44
Dima Pilipenko <dimadenver3@gmail.com>
2022-05-20 11:53:07 +0300

    added solution to hw

---
+++ src/main/java/mate/controller/CreateDriverController.java
@@ -0,0 +1,35 @@
+package mate.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.model.Driver;
+import mate.service.DriverService;
+
+@WebServlet(urlPatterns = "/drivers/create")
+public class CreateDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/drivers/create.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Driver driver = new Driver();
+        String driverName = req.getParameter("driverName");
+        String driverLicenseNumber = req.getParameter("driverLicenseNumber");
+        driver.setLicenseNumber(driverLicenseNumber);
+        driver.setName(driverName);
+        driverService.create(driver);
+        resp.sendRedirect(req.getContextPath() + "/");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/drivers/create.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Drivers</title>
+</head>
+<body>
+<h1>Create Driver</h1>
+
+<form method="post" action="${pageContext.request.contextPath}/drivers/create">
+    Name <input type="text" name="driverName"><br>
+    LicenseNumber <input type="text" name="driverLicenseNumber"><br>
+    <button type="submit">Confirm</button>
+</form>
+</body>
+</html>
