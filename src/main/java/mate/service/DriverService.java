d7efcd2027c5afb3deda745571aa88a2f72a40a1
dombrovskiy2850@gmail.com <dombrovskiy2850@gmail.com>
2022-05-23 13:45:00 +0300

    fixed mistake after mentor review

--- src/main/java/mate/service/DriverService.java
+++ src/main/java/mate/service/DriverService.java
@@ -1,10 +1,6 @@
 package mate.service;
 
-import java.util.List;
 import mate.model.Driver;
 
 public interface DriverService extends GenericService<Driver> {
-    Driver create(Driver driver);
-
-    public List<Driver> getAll();
 }
