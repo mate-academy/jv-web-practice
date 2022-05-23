cf4477166e045835d4d9f23e8e6d39ee50f1a83e
Roman Kachmaryk <roman.kv98@gmail.com>
2022-05-23 17:37:23 +0300

    code refactoring has been performed 1

--- src/main/java/mate/controller/AddManufacturerController.java
+++ src/main/java/mate/controller/AddManufacturerController.java
@@ -1,43 +1,36 @@
 package mate.controller;
 
 import java.io.IOException;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.lib.Injector;
-import mate.model.Car;
 import mate.model.Manufacturer;
-import mate.service.CarService;
 import mate.service.ManufacturerService;
 
-@WebServlet(urlPatterns = "/cars/create")
-public class CreateCarController extends HttpServlet {
+@WebServlet(urlPatterns = "/manufacturers/add")
+public class AddManufacturerController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
-    private final CarService carService
-            = (CarService) injector.getInstance(CarService.class);
     private final ManufacturerService manufacturerService
             = (ManufacturerService) injector.getInstance(ManufacturerService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/cars/create.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/manufacturers/add.jsp").forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-        Car car = new Car();
-        String model = req.getParameter("model");
-        Manufacturer manufacturer
-                = manufacturerService.get(Long.parseLong(req.getParameter("manufacturer_id")));
-        car.setModel(model);
-        car.setManufacturer(manufacturer);
-        car.setDrivers(List.of());
-        carService.create(car);
-        resp.sendRedirect(req.getContextPath() + "/cars/create");
+        Manufacturer manufacturer = new Manufacturer();
+        String name = req.getParameter("name");
+        String country = req.getParameter("country");
+        manufacturer.setName(name);
+        manufacturer.setCountry(country);
+        manufacturerService.create(manufacturer);
+        resp.sendRedirect(req.getContextPath() + "/manufacturers/add");
     }
 }
