d06658f109c2656ec6a5628ad702a35c47493d2d
dombrovskiy2850@gmail.com <dombrovskiy2850@gmail.com>
2022-05-23 18:25:55 +0300

    checkstyle

--- src/main/java/mate/dao/DriverDao.java
+++ src/main/java/mate/dao/DriverDao.java
@@ -1,6 +1,5 @@
 package mate.dao;
 
-import java.util.List;
 import mate.model.Driver;
 
 public interface DriverDao extends GenericDao<Driver> {
--- src/main/java/mate/dao/ManufacturerDao.java
+++ src/main/java/mate/dao/ManufacturerDao.java
@@ -1,6 +1,5 @@
 package mate.dao;
 
-import java.util.Optional;
 import mate.model.Manufacturer;
 
 public interface ManufacturerDao extends GenericDao<Manufacturer> {
