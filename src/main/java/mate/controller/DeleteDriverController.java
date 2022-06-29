b46348d273952142a8a03bc1ddfed89858084a1e
anton-karpov <47927921+antonEEmus@users.noreply.github.com>
2022-06-29 16:53:44 +0300

    url change
    Co-authored-by: Oleh Kvasha <61326568+Noct2000@users.noreply.github.com>

--- src/main/java/mate/controller/DeleteDriverController.java
+++ src/main/java/mate/controller/DeleteDriverController.java
@@ -21,5 +21,6 @@ public class DeleteDriverController extends HttpServlet {
         Long id = Long.valueOf(req.getParameter("id"));
         driverService.delete(id);
         resp.sendRedirect(req.getContextPath() + "/drivers/all");
+
     }
 }
