3b0730dd9c2913a0c49532466a5525eb2f92f003
anton-karpov <47927921+antonEEmus@users.noreply.github.com>
2022-06-29 16:56:05 +0300

    url change

--- src/main/java/mate/controller/DeleteManufacturerController.java
+++ src/main/java/mate/controller/DeleteManufacturerController.java
@@ -19,6 +19,6 @@ public class DeleteManufacturerController extends HttpServlet {
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         manufacturerService.delete(Long.valueOf(req.getParameter("id")));
-        resp.sendRedirect(req.getContextPath() + "/manufacturers/all");
+        resp.sendRedirect(req.getContextPath() + "/manufacturers");
     }
 }
