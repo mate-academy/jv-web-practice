3c7abccbcaced2e0c5f241f0591f7955a595f548
drapail <mv.dibrov@gmail.com>
2022-07-24 23:45:51 +0300

    addDriver webpage and backend added

---
+++ src/main/java/mate/controller/AddDriverController.java
@@ -0,0 +1,33 @@
+package mate.controller;
+
+import mate.lib.Injector;
+import mate.model.Driver;
+import mate.service.DriverService;
+import mate.service.DriverServiceImpl;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddDriverController extends HttpServlet {
+    public static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService = (DriverServiceImpl) injector.getInstance(DriverService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/addDriver.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Driver driver = new Driver();
+        driver.setLicenseNumber(req.getParameter("driverLicenseNumber"));
+        driver.setName(req.getParameter("driverName"));
+        driverService.create(driver);
+
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/addDriver.jsp
@@ -0,0 +1,16 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Adding Driver</title>
+</head>
+<body>
+<h1>Hello, mates!</h1>
+
+<form method="post" action="${pageContext.request.contextPath}/add/driver">
+  Driver License Number <input type="text" name="driverLicenseNumber">
+  Driver Name <input type="text" name="driverName">
+  <button type="submit">Post</button>
+</form>
+
+</body>
+</html>
