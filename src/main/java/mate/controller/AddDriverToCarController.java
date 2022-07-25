32e1a2e18c24c310d34a35adf6c26f14c5156bee
drapail <mv.dibrov@gmail.com>
2022-07-25 20:18:45 +0300

    Implemented adding driver to car

---
+++ src/main/java/mate/controller/AddDriverToCarController.java
@@ -0,0 +1,40 @@
+package mate.controller;
+
+import mate.lib.Injector;
+import mate.model.Car;
+import mate.model.Driver;
+import mate.service.CarService;
+import mate.service.CarServiceImpl;
+import mate.service.DriverService;
+import mate.service.DriverServiceImpl;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class AddDriverToCarController extends HttpServlet {
+    public static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService = (DriverServiceImpl) injector.getInstance(DriverService.class);
+    private final CarService carService = (CarServiceImpl) injector.getInstance(CarService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/addDriverToCar.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Driver driver = new Driver();
+        driver.setLicenseNumber(req.getParameter("driverLicenseNumber"));
+        driver.setName(req.getParameter("driverName"));
+
+        Car car = carService.get(Long.valueOf(req.getParameter("carId")));
+        driverService.create(driver);
+        carService.addDriverToCar(driver, car);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/addDriverToCar.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Adding Driver</title>
+</head>
+<body>
+<h1>Hello, mates!</h1>
+
+<form method="post" action="${pageContext.request.contextPath}/add/driverToCar">
+  Driver License Number <input type="text" name="driverLicenseNumber">
+  Driver Name <input type="text" name="driverName">
+  Car Id <input type="text" name="carId">
+  <button type="submit">Post</button>
+</form>
+
+</body>
+</html>
