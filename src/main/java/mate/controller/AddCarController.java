94f29f64c98f88098f2b575cf2148b3b5c51cf28
drapail <mv.dibrov@gmail.com>
2022-07-25 19:55:01 +0300

    Implemented creating a car

---
+++ src/main/java/mate/controller/AddCarController.java
@@ -0,0 +1,44 @@
+package mate.controller;
+
+import mate.lib.Injector;
+import mate.model.Car;
+import mate.model.Driver;
+import mate.model.Manufacturer;
+import mate.service.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddCarController extends HttpServlet {
+    public static final Injector injector = Injector.getInstance("mate");
+    private final CarService carService = (CarServiceImpl) injector.getInstance(CarService.class);
+    private final ManufacturerService manufacturerService = (ManufacturerServiceImpl) injector.getInstance(ManufacturerService.class);
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/addCar.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setName(req.getParameter("manufacturerName"));
+        manufacturer.setCountry(req.getParameter("manufacturerCountry"));
+        manufacturer = manufacturerService.create(manufacturer);
+
+        Car car = new Car();
+        car.setManufacturer(manufacturer);
+        car.setModel(req.getParameter("carModel"));
+        car.setDrivers(new ArrayList<Driver>());
+
+        carService.create(car);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/addCar.jsp
@@ -0,0 +1,17 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Adding Car</title>
+</head>
+<body>
+<h1>Hello, mates!</h1>
+
+<form method="post" action="${pageContext.request.contextPath}/add/car">
+  Car Model <input type="text" name="carModel">
+  Manufacturer Name <input type="text" name="manufacturerName">
+  Manufacturer Country <input type="text" name="manufacturerCountry">
+  <button type="submit">Post</button>
+</form>
+
+</body>
+</html>
