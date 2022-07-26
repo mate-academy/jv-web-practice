bb020fbcd8bd6b098e066a74c795200fcaa101ea
Illia Khomych <illia.homych@knu.ua>
2022-07-26 15:32:24 +0300

    Homework task implemented

---
+++ src/main/java/mate/controller/CreateDriverController.java
@@ -0,0 +1,35 @@
+package mate.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.model.Driver;
+import mate.service.DriverService;
+
+public class CreateDriverController extends HttpServlet {
+    private final DriverService driverService;
+
+    {
+        driverService = (DriverService) Injector.getInstance("mate")
+                .getInstance(DriverService.class);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/createDriver.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String name = req.getParameter("name");
+        String licenseNumber = req.getParameter("licenseNumber");
+        Driver driver = new Driver(name, licenseNumber);
+        driverService.create(driver);
+        req.getRequestDispatcher("/WEB-INF/views/done.jsp").forward(req, resp);
+    }
+}
