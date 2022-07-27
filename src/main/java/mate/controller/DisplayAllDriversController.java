a43431100e8f954af58094023d682cd510340089
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-28 01:25:55 +0300

    Added logic to DisplayAllDriversController

--- src/main/java/mate/controller/DisplayAllDriversController.java
+++ src/main/java/mate/controller/DisplayAllDriversController.java
@@ -1,15 +1,26 @@
 package mate.controller;
 
+import mate.lib.Injector;
+import mate.model.Driver;
+import mate.service.DriverService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class DisplayAllDriversController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService =
+            (DriverService) injector.getInstance(DriverService.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        List<Driver> drivers = driverService.getAll();
+        req.setAttribute("drivers", drivers);
         req.getRequestDispatcher("/WEB-INF/views/drivers/drivers.jsp").forward(req, resp);
     }
 }
