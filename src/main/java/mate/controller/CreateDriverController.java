6441e0c372ee9e302c3f90e799eecff33af5a8ee
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-28 01:24:54 +0300

    Added logic to CreateDriverController

--- src/main/java/mate/controller/CreateDriverController.java
+++ src/main/java/mate/controller/CreateDriverController.java
@@ -1,5 +1,9 @@
 package mate.controller;
 
+import mate.lib.Injector;
+import mate.model.Driver;
+import mate.service.DriverService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,9 +11,25 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CreateDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService =
+            (DriverService) injector.getInstance(DriverService.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/views/drivers/create.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        String driverName = req.getParameter("name");
+        String driverLicenseNumber = req.getParameter("licenseNumber");
+        Driver driver = new Driver();
+        driver.setName(driverName);
+        driver.setLicenseNumber(driverLicenseNumber);
+        driverService.create(driver);
+        resp.sendRedirect(req.getContextPath() + "/drivers/drivers");
     }
 }
