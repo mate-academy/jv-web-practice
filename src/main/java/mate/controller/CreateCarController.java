071d000803cc7be64329299c566336e22481a082
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-28 12:45:50 +0300

    Refactored some code in import blocks

--- src/main/java/mate/controller/CreateCarController.java
+++ src/main/java/mate/controller/CreateCarController.java
@@ -1,12 +1,12 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import mate.lib.Injector;
 import mate.model.Car;
 import mate.model.Manufacturer;
