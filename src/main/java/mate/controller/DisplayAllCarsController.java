a5a9654969c06a92d91b789948244d3d1d7ac48a
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-28 11:27:43 +0300

    Added attributes for request in doGet method

--- src/main/java/mate/controller/DisplayAllCarsController.java
+++ src/main/java/mate/controller/DisplayAllCarsController.java
@@ -1,5 +1,9 @@
 package mate.controller;
 
+import mate.lib.Injector;
+import mate.service.CarService;
+import mate.service.DriverService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,9 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class DisplayAllCarsController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final CarService carsService =
+            (CarService) injector.getInstance(CarService.class);
+    private final DriverService driverService =
+            (DriverService) injector.getInstance(DriverService.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        req.setAttribute("cars", carsService.getAll());
+        req.setAttribute("drivers", driverService.getAll());
         req.getRequestDispatcher("/WEB-INF/views/cars/cars.jsp").forward(req, resp);
     }
 }
