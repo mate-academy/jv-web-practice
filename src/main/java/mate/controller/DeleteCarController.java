f2ae67b2684fbbca69d11a20ca5efdc31d84d8fd
Oleksii Mospan <alexey.mospan@gmail.com>
2022-07-28 12:03:25 +0300

    Added logic for deleting car

--- src/main/java/mate/controller/DeleteCarController.java
+++ src/main/java/mate/controller/DeleteCarController.java
@@ -1,5 +1,9 @@
 package mate.controller;
 
+import mate.lib.Injector;
+import mate.service.CarService;
+import mate.service.DriverService;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,9 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class DeleteCarController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final CarService carsService =
+            (CarService) injector.getInstance(CarService.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/cars/cars.jsp").forward(req, resp);
+        Long id = Long.valueOf(req.getParameter("car_id"));
+        carsService.delete(id);
+        resp.sendRedirect(req.getContextPath() + "/cars/cars");
     }
 }
