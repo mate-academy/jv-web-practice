f89b5ce32fff7fbfb3907dc1d0464d867a054030
yaroslav <morozov.yrv@gmail.com>
2022-08-01 16:31:10 +0300

    DeleteCarController ready

---
+++ src/main/java/mate/controller/DeleteCarController.java
@@ -0,0 +1,22 @@
+package mate.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.service.CarService;
+
+public class DeleteCarController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final CarService carService = (CarService) injector.getInstance(CarService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        long carId = Long.parseLong(req.getParameter("carId"));
+        carService.delete(carId);
+        resp.sendRedirect(req.getContextPath() + "/cars");
+    }
+}
