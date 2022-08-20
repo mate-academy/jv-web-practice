1e76349f74ed7c448070dd6177412f1fc37ac32d
Vladimir Chuvashov <vova5179@gmail.com>
2022-08-20 11:21:21 +0300

    renamed addDriver.jsp to /cars/drivers/add.jsp

--- src/main/java/mate/controller/AddDriverToCarController.java
+++ src/main/java/mate/controller/AddDriverToCarController.java
@@ -22,7 +22,7 @@ public class AddDriverToCarController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/cars/addDriver.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/cars/drivers/addadd.jsp").forward(req, resp);
     }
 
     @Override
