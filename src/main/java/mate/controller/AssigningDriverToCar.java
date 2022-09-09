f5ee8b01bbfd985c86da89e443fdf776b6e98d0d
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-09-09 13:52:22 +0300

    Homework is developed

---
+++ src/main/java/mate/controller/AssigningDriverToCar.java
@@ -0,0 +1,55 @@
+package mate.controller;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.model.Car;
+import mate.model.Driver;
+import mate.service.CarService;
+import mate.service.DriverService;
+
+@WebServlet(urlPatterns = "/cars/add_driver")
+public class AssigningDriverToCar extends HttpServlet {
+    private DriverService driverService;
+    private CarService carService;
+    private List<Car> cars;
+    private List<Driver> drivers;
+
+    @Override
+    public void init() throws ServletException {
+        Injector injector = Injector.getInstance("mate");
+        driverService = (DriverService) injector.getInstance(DriverService.class);
+        carService = (CarService) injector.getInstance(CarService.class);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        drivers = driverService.getAll();
+        cars = carService.getAll();
+        req.setAttribute("drivers", drivers);
+        req.setAttribute("cars", cars);
+        req.getRequestDispatcher("/WEB-INF/views/cars/assigningDriverToCar.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Car car = cars.stream()
+                .filter(c -> Objects.equals(c.getModel(), req.getParameter("car")))
+                .findFirst().get();
+
+        Driver driver = drivers.stream()
+                .filter(d -> Objects.equals(d.getName(), req.getParameter("driver")))
+                .findFirst().get();
+
+        carService.addDriverToCar(driver, car);
+        resp.sendRedirect("/index");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/cars/assigningDriverToCar.jsp
@@ -0,0 +1,24 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Add Driver</title>
+</head>
+<body>
+<form method="post" action="${pageContext.request.contextPath}/cars/add_driver">
+  Driver:
+  <select name="driver">
+    <c:forEach items="${drivers}" var="driver">
+      <option><c:out value="${driver.name}"/></option>
+    </c:forEach>
+  </select><br>
+  assign to the Car:
+  <select name="car">
+    <c:forEach items="${cars}" var="car">
+      <option><c:out value="${car.model}"/></option>
+    </c:forEach>
+  </select><br>
+  <button type="submit">Assign</button>
+</form>
+</body>
+</html>
