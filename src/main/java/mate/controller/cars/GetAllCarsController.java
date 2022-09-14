1e52091783445cdce572ddcf9dc7fb9366a6bcd8
Valerii-Sh <val-sh@ukr.net>
2022-09-14 18:44:45 +0300

    HW jv-web-practice is done.

---
+++ src/main/java/mate/controller/cars/GetAllCarsController.java
@@ -0,0 +1,24 @@
+package mate.controller.cars;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.service.CarService;
+
+@WebServlet(urlPatterns = "/cars")
+public class GetAllCarsController extends HttpServlet {
+    private static final Injector INJECTOR = Injector.getInstance("mate");
+    private final CarService carService
+            = (CarService) INJECTOR.getInstance(CarService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.setAttribute("cars", carService.getAll());
+        req.getRequestDispatcher("/WEB-INF/views/cars/all.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/drivers/all.jsp
@@ -0,0 +1,26 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Taxi service: all drivers<</title>
+</head>
+<body>
+<h1>List drivers:</h1>
+<table>
+    <tr>
+        <td>id</td>
+        <td>name</td>
+        <td>license_number</td>
+        <td>Action</td>
+    </tr>
+    <c:forEach items="${drivers}" var="driver">
+        <tr>
+            <td><c:out value="${driver.id}" /></td>
+            <td><c:out value="${driver.name}" /></td>
+            <td><c:out value="${driver.licenseNumber}" /></td>
+            <td><a href="${pageContext.request.contextPath}/drivers/delete?id=${driver.id}">Delete this driver</a> </td>
+        </tr>
+    </c:forEach>
+</table>
+</body>
+</html>
