142ac3b4b17721e8efb98dd66743ccc4d564d6a4
Vasyl Pankiv <wasilius2@gmail.com>
2022-09-19 17:07:43 +0300

    add DeleteDriverController

---
+++ src/main/java/mate/controller/driver/DeleteDriverController.java
@@ -0,0 +1,26 @@
+package mate.controller.driver;
+
+import mate.lib.Injector;
+import mate.service.DriverService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/drivers/delete")
+public class DeleteDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService =
+            (DriverService) injector.getInstance(DriverService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Long carId = Long.valueOf(req.getParameter("id"));
+        driverService.delete(carId);
+        resp.sendRedirect(req.getContextPath() + "/drivers");
+    }
+}
--- src/main/webapp/WEB-INF/views/drivers/drivers.jsp
+++ src/main/webapp/WEB-INF/views/drivers/drivers.jsp
@@ -12,16 +12,13 @@
     <td>Name</td>
     <td>LicenseNumber</td>
     <td>delete driver</td>
-    <form action="${pageContext.request.contextPath}/index">
-      <input type="submit" value="Return to First page">
-    </form>
   </tr>
   <c:forEach items="${drivers}" var="driver">
     <tr>
       <td><c:out value="${driver.id}"/></td>
       <td><c:out value="${driver.name}"/></td>
       <td><c:out value="${driver.licenseNumber}"/></td>
-      <td><a href="${pageContext.request.contextPath}/deleteDriver?id=${driver.id}">delete</a></td>
+      <td><a href="${pageContext.request.contextPath}/drivers/delete?id=${driver.id}">delete</a></td>
     </tr>
   </c:forEach>
 </table>
