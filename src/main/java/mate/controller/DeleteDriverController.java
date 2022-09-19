de08a39dd2a3ab238f90b4b4aaadb6b44f8d02a0
buuufy <s1mpleworkpost@gmail.com>
2022-09-19 03:27:39 +0300

    add syntax fixes

--- src/main/java/mate/controller/DeleteDriverController.java
+++ src/main/java/mate/controller/DeleteDriverController.java
@@ -1,13 +1,13 @@
 package mate.controller;
 
-import mate.lib.Injector;
-import mate.service.DriverService;
+import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import mate.lib.Injector;
+import mate.service.DriverService;
 
 @WebServlet(urlPatterns = "/deleteDriver")
 public class DeleteDriverController extends HttpServlet {
--- src/main/java/mate/controller/DisplayAllManufacturersController.java
+++ src/main/java/mate/controller/DisplayAllManufacturersController.java
@@ -1,13 +1,13 @@
 package mate.controller;
 
-import mate.lib.Injector;
-import mate.service.ManufacturerService;
+import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import mate.lib.Injector;
+import mate.service.ManufacturerService;
 
 @WebServlet(urlPatterns = "/displayAllManufacturers")
 public class DisplayAllManufacturersController extends HttpServlet {
