e84667265ce9c1ff803af8901bcb31d390c2bf9a
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-09-19 17:28:16 +0200

    implement DeleteDriverController

--- src/main/java/mate/controller/driver/DeleteDriverController.java
+++ src/main/java/mate/controller/driver/DeleteDriverController.java
@@ -1,5 +1,7 @@
 package mate.controller.driver;
 
+import mate.lib.Injector;
+import mate.service.DriverService;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,9 +11,15 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = "/drivers/delete")
 public class DeleteDriverController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService =
+            (DriverService) injector.getInstance(DriverService.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        super.doGet(req, resp);
+        Long id = Long.valueOf(req.getParameter("id"));
+        driverService.delete(id);
+        resp.sendRedirect(req.getContextPath() + "/drivers");
     }
 }
