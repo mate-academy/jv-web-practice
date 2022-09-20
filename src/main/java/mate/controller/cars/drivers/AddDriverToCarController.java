b19e3917cc00ccde60586934cf1f0f543324ec8b
Veers <raadch3011@gmail.com>
2022-09-20 11:42:50 +0300

    Create controllers to create/delete/get all manufacturers/drivers/cars, create jsp files

--- src/main/java/mate/controller/cars/drivers/AddDriverToCarController.java
+++ src/main/java/mate/controller/cars/drivers/AddDriverToCarController.java
@@ -1,22 +1,25 @@
 package mate.controller.cars.drivers;
 
-import mate.lib.Injector;
-import mate.model.Car;
-import mate.model.Driver;
-import mate.service.CarService;
-import mate.service.DriverService;
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import mate.lib.Injector;
+import mate.model.Car;
+import mate.model.Driver;
+import mate.service.CarService;
+import mate.service.DriverService;
 
 @WebServlet(urlPatterns = "/cars/drivers/add")
 public class AddDriverToCarController extends HttpServlet {
-    private static final Injector injector = Injector.getInstance("mate.academy"); // be careful with copy-paste
-    private final CarService carService = (CarService) injector.getInstance(CarService.class);
-    private final DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
+    private static final Injector injector
+            = Injector.getInstance("mate.academy"); // be careful with copy-paste
+    private final CarService carService
+            = (CarService) injector.getInstance(CarService.class);
+    private final DriverService driverService
+            = (DriverService) injector.getInstance(DriverService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
