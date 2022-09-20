a6c5c2ee21d7580f75a225d440694ebcf7132c4f
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-09-20 16:24:23 +0300

    init DB

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -11,12 +11,11 @@ CREATE TABLE `drivers`  (
                             `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT,
                             `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
                             `license_number` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                            `deleted` bit(1) NOT NULL DEFAULT b'0',
                             `is_deleted` bit(1) NOT NULL DEFAULT b'0',
                             PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-INSERT INTO `drivers` (`id`, `name`, `license_number`) VALUES
-						(1,'Ihor','123456789'),
-						(2,'Oleg','852741963');
+
 -- ----------------------------
 -- Table structure for manufacturers
 -- ----------------------------
@@ -25,27 +24,11 @@ CREATE TABLE `manufacturers`  (
                                   `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT,
                                   `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
                                   `country` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+                                  `deleted` bit(1) NOT NULL DEFAULT b'0',
                                   `is_deleted` bit(1) NOT NULL DEFAULT b'0',
                                   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-INSERT INTO `cars` (`manufacturer_id`, `model`) VALUES 
-										('3', 'A3'), 
-										('3', 'Q7'),
-										('4', 'X5'),
-										('4', 'X3');
 
-INSERT INTO `manufacturers` (`id`, `name`, `country`) VALUES
-							(1,'Acura','Japan'),
-							(2,'Alfa Romeo','Italy'),
-							(3,'Audi','Germany'),
-							(4,'BMW','Germany'),
-							(5,'Ferrari','Italy'),
-							(6,'SEAT','Spain'),
-							(7,'Renault','France'),
-							(8,'Peugeot','France'),
-							(9,'Fiat','Italy'),
-							(10,'Honda','Japan'),
-							(11,'Lexus','Japan');
 -- ----------------------------
 -- Table structure for cars
 -- ----------------------------
@@ -54,16 +37,13 @@ CREATE TABLE `cars`  (
                          `id` bigint(0) UNSIGNED NOT NULL AUTO_INCREMENT,
                          `model` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
                          `manufacturer_id` bigint(0) UNSIGNED NOT NULL,
+                         `deleted` bit(1) NOT NULL DEFAULT b'0',
                          `is_deleted` bit(1) NOT NULL DEFAULT b'0',
                          PRIMARY KEY (`id`) USING BTREE,
                          INDEX `FK_manufacturer_id`(`manufacturer_id`) USING BTREE,
                          CONSTRAINT `FK_manufacturer_id` FOREIGN KEY (`manufacturer_id`) REFERENCES `manufacturers` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-INSERT INTO `taxi`.`cars` (`manufacturer_id`, `model`) VALUES 
-										('3', 'A3'), 
-										('3', 'Q7'),
-										('4', 'X5'),
-										('4', 'X3');
+
 -- ----------------------------
 -- Table structure for cars_drivers
 -- ----------------------------
@@ -77,21 +57,5 @@ CREATE TABLE `cars_drivers`  (
                                  CONSTRAINT `car_id` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
                                  CONSTRAINT `driver_id` FOREIGN KEY (`driver_id`) REFERENCES `drivers` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-INSERT INTO `taxi`.`cars_drivers` (`car_id`, `driver_id`) VALUES  
-											(2, 1),
-                                            (3, 2),
-                                            (1, 3);
-                                            
+
 SET FOREIGN_KEY_CHECKS = 1;
-
-
--- ALTER TABLE `manufacturers` AUTO_INCREMENT = 1;
-SELECT * FROM taxi.manufacturers;
-SELECT * FROM taxi.cars;
-SELECT * FROM taxi.drivers;
-SELECT * FROM taxi.cars_drivers;
-
-TRUNCATE `taxi`.`cars_drivers`;
-TRUNCATE `taxi`.`drivers`;
-TRUNCATE `taxi`.`cars`;
-TRUNCATE `taxi`.`manufacturers`;
