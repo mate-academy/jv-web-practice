45a63e3ca3142401771ed859181da95b8b05540f
Ilja Nekrituk <Nekrit99@gmail.com>
2022-09-20 16:48:13 +0300

    Fixed!

--- src/main/java/mate/controller/car/GetAllCarsController.java
+++ src/main/java/mate/controller/car/GetAllCarsController.java
@@ -12,8 +12,8 @@ import mate.service.CarService;
 @WebServlet(urlPatterns = "/cars")
 public class GetAllCarsController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
-    private final CarService carService
-            = (CarService) injector.getInstance(CarService.class);
+    private final CarService carService =
+            (CarService) injector.getInstance(CarService.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
