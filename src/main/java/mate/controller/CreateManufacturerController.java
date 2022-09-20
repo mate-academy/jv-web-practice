64981720692f5faf30c12ef8e10859a78238a619
Misha Brovkin <haasiakira@gmail.com>
2022-09-20 13:34:14 +0300

    created taxi service

---
+++ src/main/java/mate/controller/CreateManufacturerController.java
@@ -0,0 +1,36 @@
+package mate.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.model.Manufacturer;
+import mate.service.ManufacturerService;
+
+@WebServlet(urlPatterns = "/manufacturers/add")
+public class CreateManufacturerController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private static final ManufacturerService manufacturerService = (ManufacturerService) injector
+            .getInstance(ManufacturerService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/manufacturers/create.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        String name = req.getParameter("name");
+        String country = req.getParameter("country");
+        Manufacturer manufacturer = new Manufacturer();
+        manufacturer.setName(name);
+        manufacturer.setCountry(country);
+        manufacturerService.create(manufacturer);
+        resp.sendRedirect(req.getContextPath() + "/manufacturers");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/manufacturers/all.jsp
@@ -0,0 +1,30 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>All manufacturers</title>
+</head>
+<body>
+<table>
+  <tr>
+    <td>Id</td>
+    <td>Name</td>
+    <td>Country</td>
+  </tr>
+  <c:forEach items="${manufacturers}" var="manufacturer">
+    <tr>
+      <td><c:out value="${manufacturer.getId()}" /></td>
+      <td><c:out value="${manufacturer.getName()}" /></td>
+      <td><c:out value="${manufacturer.getCountry()}" /></td>
+      <td>
+        <a href="${pageContext.request.contextPath}/manufacturers/delete?id=${manufacturer
+                .getId()}"><button>delete this manufacturer</button></a>
+      </td>
+    </tr>
+  </c:forEach>
+  <form action="${pageContext.request.contextPath}/manufacturers/add">
+    <input type="submit" value="Add new Manufacturer">
+  </form>
+</table>
+</body>
+</html>
