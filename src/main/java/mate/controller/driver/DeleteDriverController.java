1931ac2e3552b495188ab15b4cd4707db87561eb
Vasyl Pankiv <wasilius2@gmail.com>
2022-09-20 06:23:14 +0300

    add AddDriverController

--- src/main/java/mate/controller/driver/DeleteDriverController.java
+++ src/main/java/mate/controller/driver/DeleteDriverController.java
@@ -19,8 +19,7 @@ public class DeleteDriverController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        Long carId = Long.valueOf(req.getParameter("id"));
-        driverService.delete(carId);
+        driverService.delete(Long.valueOf(req.getParameter("id")));
         resp.sendRedirect(req.getContextPath() + "/drivers");
     }
 }
