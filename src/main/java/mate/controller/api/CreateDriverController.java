c434f8ab8be4582b5bfcabebedc542552197db8a
shmax <mshveda28@gmail.com>
2022-11-07 15:04:24 +0200

    Rename DriverCreateController to CreateDriverController

--- src/main/java/mate/controller/api/CreateDriverController.java
+++ src/main/java/mate/controller/api/CreateDriverController.java
@@ -11,7 +11,7 @@ import mate.model.Driver;
 import mate.service.DriverService;
 
 @WebServlet(urlPatterns = {"/api/driver/new"})
-public class DriverCreateController extends HttpServlet {
+public class CreateDriverController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
     private final DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
 
