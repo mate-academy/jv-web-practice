85b4cc829c6897cc2b153efa4db2a31640c98ec4
shmax <mshveda28@gmail.com>
2022-11-07 15:30:08 +0200

    Rename url pattern for creating drivers

--- src/main/java/mate/controller/api/CreateDriverController.java
+++ src/main/java/mate/controller/api/CreateDriverController.java
@@ -10,7 +10,7 @@ import mate.lib.Injector;
 import mate.model.Driver;
 import mate.service.DriverService;
 
-@WebServlet(urlPatterns = {"/api/driver/new"})
+@WebServlet(urlPatterns = {"/api/drivers/new"})
 public class CreateDriverController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
     private final DriverService driverService = (DriverService) injector.getInstance(DriverService.class);
