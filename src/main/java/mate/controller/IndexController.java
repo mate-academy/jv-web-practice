60498bf9cd3a6d98c2cd22aa2492cc1d3c40e6c3
AntonPav <streetreadger@gmail.com>
2022-11-10 22:23:26 +0200

    Correct connection to DB

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,6 +1,9 @@
 package mate.controller;
 
+import mate.util.ConnectionUtil;
+
 import java.io.IOException;
+import java.sql.Connection;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +13,7 @@ public class IndexController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        Connection connection = ConnectionUtil.getConnection();
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
