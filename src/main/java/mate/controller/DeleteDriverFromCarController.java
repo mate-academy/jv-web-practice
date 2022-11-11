d6b3ad4edfb327e6f1fa43c98744aaec2801a9b4
shmax <mshveda28@gmail.com>
2022-11-11 12:10:30 +0200

    Rename RemoveDriverFromCarController to DeleteDriverFromCarController

--- src/main/java/mate/controller/DeleteDriverFromCarController.java
+++ src/main/java/mate/controller/DeleteDriverFromCarController.java
@@ -12,7 +12,7 @@ import mate.service.CarService;
 import mate.service.DriverService;
 
 @WebServlet(urlPatterns = "/cars/drivers/delete")
-public class RemoveDriverFromCarController extends HttpServlet {
+public class DeleteDriverFromCarController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
     private final CarService carService = (CarService) injector.getInstance(CarService.class);
     private final DriverService driverService
