db50f4d0a1cdb3cef47bb4fab2be188a903f3ff8
Roman Denysiuk <r.v.denysiuk@gmail.com>
2022-11-16 23:54:41 +0200

    checkstyle changes

--- src/main/java/mate/controller/IndexController.java
+++ src/main/java/mate/controller/IndexController.java
@@ -1,12 +1,10 @@
 package mate.controller;
 
 import java.io.IOException;
-import java.sql.Connection;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import mate.util.ConnectionUtil;
 
 public class IndexController extends HttpServlet {
     @Override
