2a48955d514ca884c8df7451d2bdb6475dfc83a9
Anastasia Vasylyk <anastasiavasylykv@gmail.com>
2022-11-22 15:52:12 +0200

    Added some style fixes

--- src/main/java/mate/controller/car/CreateCarController.java
+++ src/main/java/mate/controller/car/CreateCarController.java
@@ -1,7 +1,6 @@
 package mate.controller.car;
 
 import java.io.IOException;
-import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.lib.Injector;
 import mate.model.Car;
-import mate.model.Driver;
 import mate.model.Manufacturer;
 import mate.service.CarService;
 import mate.service.DriverService;
