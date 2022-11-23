fdf12073833321ee4f308d0332bf04f9e509c75b
Oleksandr <dovy_al@ukr.net>
2022-11-23 13:27:27 +0200

    Create a new controller: CreateCarController

--- src/main/java/mate/controller/CreateCarController.java
+++ src/main/java/mate/controller/CreateCarController.java
@@ -1,18 +1,16 @@
 package mate.controller;
 
-import mate.lib.Injector;
-import mate.model.Car;
-import mate.model.Manufacturer;
-import mate.service.CarService;
-import mate.service.ManufacturerService;
-
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
+import mate.lib.Injector;
+import mate.model.Car;
+import mate.model.Manufacturer;
+import mate.service.CarService;
+import mate.service.ManufacturerService;
 
 @WebServlet(urlPatterns = "/cars/create")
 public class CreateCarController extends HttpServlet {
@@ -21,8 +19,10 @@ public class CreateCarController extends HttpServlet {
             (CarService) injector.getInstance(CarService.class);
     private final ManufacturerService manufacturerService =
             (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         String model = req.getParameter("model");
         Long manufacturerId = Long.valueOf(req.getParameter("manufacturerId"));
         Manufacturer manufacturer =
@@ -35,7 +35,8 @@ public class CreateCarController extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/views/cars/create.jsp").forward(req, resp);
     }
 }
