6b72153385e1c19170d275971baeb39635df1118
Yaroslav Pelekhach <108753784+Yaroslav781@users.noreply.github.com>
2022-11-26 00:09:24 +0200

    fixed orders of imports

--- src/main/java/mate/controller/car/CreateCarController.java
+++ src/main/java/mate/controller/car/CreateCarController.java
@@ -1,17 +1,16 @@
 package mate.controller.car;
 
-import mate.lib.Injector;
-import mate.model.Car;
-import mate.service.CarService;
-import mate.service.ManufacturerService;
-
+import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
+import mate.lib.Injector;
+import mate.model.Car;
+import mate.service.CarService;
+import mate.service.ManufacturerService;
 
 @WebServlet(urlPatterns = "/cars/create")
 public class CreateCarController extends HttpServlet {
