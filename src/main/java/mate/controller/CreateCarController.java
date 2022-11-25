41d7c5c43f208747d67979952710081f8052ef2e
Serhii-Prykhodko <sergii.prykhodko@gmail.com>
2022-11-25 16:38:16 +0200

    mvn fix

--- src/main/java/mate/controller/CreateCarController.java
+++ src/main/java/mate/controller/CreateCarController.java
@@ -1,6 +1,5 @@
 package mate.controller;
 
-
 import java.io.IOException;
 import java.util.Collections;
 import javax.servlet.ServletException;
--- src/main/java/mate/controller/CreateManufacturer.java
+++ src/main/java/mate/controller/CreateManufacturer.java
@@ -1,21 +1,21 @@
 package mate.controller;
 
-import mate.lib.Injector;
-import mate.model.Manufacturer;
-import mate.service.ManufacturerService;
-
+import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import mate.lib.Injector;
+import mate.model.Manufacturer;
+import mate.service.ManufacturerService;
 
 @WebServlet(urlPatterns = "/manufacturers/add")
 public class CreateManufacturer extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
     private final ManufacturerService manufacturerService =
             (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
