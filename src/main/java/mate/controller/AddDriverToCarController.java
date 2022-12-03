3c08ab8924a8c445974d7f47d965a2c8a4abf89a
DY-JS <delo1604@ukr.net>
2022-12-03 18:47:12 +0200

    Solution web-practice

--- src/main/java/mate/controller/AddDriverToCarController.java
+++ src/main/java/mate/controller/AddDriverToCarController.java
@@ -2,7 +2,6 @@ package mate.controller;
 
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
