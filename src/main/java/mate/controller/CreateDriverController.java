4ef9793a455b483cf96533085e4f33a49d8acd02
AndriySichniy <sichniy.andriy@gmail.com>
2022-12-15 21:04:39 +0200

    fixes and refactor

--- src/main/java/mate/controller/CreateDriverController.java
+++ src/main/java/mate/controller/CreateDriverController.java
@@ -3,13 +3,11 @@ package mate.controller;
 import mate.lib.Injector;
 import mate.model.Driver;
 import mate.service.DriverService;
-import mate.service.DriverServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
 
 public class CreateDriverController extends HttpServlet {
@@ -19,15 +17,15 @@ public class CreateDriverController extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("WEB-INF/views/create/driver.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/create/driver.jsp").forward(req, resp);
     }
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        Driver driver = (Driver) injector.getInstance(Driver.class);
+        Driver driver = new Driver();
         driver.setName(req.getParameter("driver_name"));
         driver.setLicenseNumber(req.getParameter("license_number"));
         driverService.create(driver);
-        resp.sendRedirect("../index.jsp");
+        resp.sendRedirect("/index");
     }
 }
--- src/main/webapp/WEB-INF/views/create/driver.jsp
+++ src/main/webapp/WEB-INF/views/create/driver.jsp
@@ -1,14 +1,21 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Андрій
+  Date: 15.12.2022
+  Time: 19:37
+  To change this template use File | Settings | File Templates.
+--%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>Create driver</title>
-  </head>
-  <body>
+    <head>
+        <title>Create driver</title>
+    </head>
+    <body>
     <h2><i>Fill the form below</i></h2>
     <form method="post" action="${pageContext.request.contextPath}/create/driver">
-      Name <input type="text" name="driver_name"> <br>
-      License number <input type="text" name="license_number"> <br>
-      <button type="submit">Create driver</button>
+        Name <input type="text" name="driver_name"> <br>
+        License number <input type="text" name="license_number"> <br>
+        <button type="submit">Create driver</button>
     </form>
-  </body>
+    </body>
 </html>
