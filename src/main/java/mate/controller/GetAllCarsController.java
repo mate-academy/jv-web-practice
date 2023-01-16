4fb927ac8cdd4d658d93fbbf7f96ad2d5de00002
Kateryna Takhtai <ktakhtay22@gmail.com>
2023-01-16 11:53:07 +0100

    web-practice solution.changes

--- src/main/java/mate/controller/GetAllCarsController.java
+++ src/main/java/mate/controller/GetAllCarsController.java
@@ -1,11 +1,11 @@
 package mate.controller;
 
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 import mate.lib.Injector;
 import mate.model.Car;
 import mate.service.CarService;
