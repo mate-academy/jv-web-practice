0c96418f28c939a45878e100f649f0cd5c3c1c49
Maksym Tymoshchenko <timoschenkomaks123@gmail.com>
2023-01-23 17:07:20 +0200

    fixed mistake

--- src/main/java/mate/controller/manufacturer/CreateManufacturerController.java
+++ src/main/java/mate/controller/manufacturer/CreateManufacturerController.java
@@ -20,7 +20,7 @@ public class CreateManufacturerController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        req.getRequestDispatcher("/WEB-INF/views/manufacturer/crate.jsp").forward(req, resp);
+        req.getRequestDispatcher("/WEB-INF/views/manufacturer/create.jsp").forward(req, resp);
     }
 
     @Override
