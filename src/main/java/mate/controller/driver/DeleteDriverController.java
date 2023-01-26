1eb4ca2ef3f572f19fd3980ce60d055f8aa77e1c
Uka0001 <ukraine0001@gmail.com>
2023-01-26 18:19:56 +0200

    fix delete doget redirect naming

--- src/main/java/mate/controller/driver/DeleteDriverController.java
+++ src/main/java/mate/controller/driver/DeleteDriverController.java
@@ -20,6 +20,6 @@ public class DeleteDriverController extends HttpServlet {
     protected void doGet(HttpServletRequest req,
                          HttpServletResponse resp) throws ServletException, IOException {
         driverService.delete(Long.valueOf(req.getParameter("id")));
-        resp.sendRedirect(req.getContextPath() + "/drivers/drivers");
+        resp.sendRedirect(req.getContextPath() + "/drivers");
     }
 }
