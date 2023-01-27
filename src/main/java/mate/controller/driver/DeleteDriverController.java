d2fb0a9800ea0ee67f039dcba3aefe879ed049b0
Uka0001 <ukraine0001@gmail.com>
2023-01-27 16:59:31 +0200

    fix all delete controllers methods

--- src/main/java/mate/controller/driver/DeleteDriverController.java
+++ src/main/java/mate/controller/driver/DeleteDriverController.java
@@ -1,7 +1,6 @@
 package mate.controller.driver;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,7 +17,7 @@ public class DeleteDriverController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req,
-                         HttpServletResponse resp) throws ServletException, IOException {
+                         HttpServletResponse resp) throws IOException {
         driverService.delete(Long.valueOf(req.getParameter("id")));
         resp.sendRedirect(req.getContextPath() + "/drivers");
     }
--- src/main/java/mate/controller/manufacturer/DeleteManufacturerController.java
+++ src/main/java/mate/controller/manufacturer/DeleteManufacturerController.java
@@ -1,7 +1,6 @@
 package mate.controller.manufacturer;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,7 +16,7 @@ public class DeleteManufacturerController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req,
-                         HttpServletResponse resp) throws ServletException, IOException {
+                         HttpServletResponse resp) throws IOException {
         manufacturerService.delete(Long.valueOf(req.getParameter("id")));
         resp.sendRedirect(req.getContextPath() + "/manufacturers");
     }
