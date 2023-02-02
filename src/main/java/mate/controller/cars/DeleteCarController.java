1a4865dd75e9e71810553aa2fc19ff8a252cd4e7
ydikalo <dikalotel@gmail.com>
2023-02-02 21:40:43 +0200

    checkstyle fixes

--- src/main/java/mate/controller/cars/DeleteCarController.java
+++ src/main/java/mate/controller/cars/DeleteCarController.java
@@ -1,14 +1,13 @@
 package mate.controller.cars;
 
-import mate.lib.Injector;
-import mate.service.CarService;
-import mate.service.ManufacturerService;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.service.CarService;
 
 @WebServlet(urlPatterns = "/cars/delete")
 public class DeleteCarController extends HttpServlet {
--- src/main/java/mate/util/ConnectionUtil.java
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -1,7 +1,6 @@
 package mate.util;
 
 import com.mysql.cj.jdbc.Driver;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
