d015c45efc9605a462a3eeb6c5b9436724afbd83
Sofiia <sofiakazzz@gmail.com>
2023-02-09 21:16:02 +0200

    Was created new controllers

---
+++ src/main/java/mate/controller/CreateCarController.java
@@ -0,0 +1,38 @@
+package mate.controller;
+
+import java.io.IOException;
+import java.util.Collections;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.model.Car;
+import mate.model.Manufacturer;
+import mate.service.CarService;
+import mate.service.ManufacturerService;
+
+@WebServlet("/cars/addCars")
+public class CreateCarController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final CarService carService = (CarService) injector.getInstance(CarService.class);
+    private final ManufacturerService manufacturerService = (ManufacturerService) injector
+            .getInstance(ManufacturerService.class);
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/car/addCars.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String model = req.getParameter("model");
+        Manufacturer manufacturerId = manufacturerService.get(Long
+                .valueOf(req.getParameter("manufacturer_id")));
+        carService.create(new Car(model, manufacturerId, Collections.emptyList()));
+        resp.sendRedirect(req.getContextPath() + "/car");
+    }
+}
---
+++ src/main/java/mate/controller/DisplayAllDriversController.java
@@ -0,0 +1,33 @@
+package mate.controller;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.model.Driver;
+import mate.service.DriverService;
+
+@WebServlet("/drivers/display/all")
+public class DisplayAllDriversController extends HttpServlet {
+    public static final Injector injector = Injector.getInstance("mate");
+    private final DriverService driverService = (DriverService) injector
+            .getInstance(DriverService.class);
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        List<Driver> drivers = driverService.getAll();
+        req.setAttribute("drivers", "drivers");
+        req.getRequestDispatcher("/WEB-INF/views/car/displayAllDrivers.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
