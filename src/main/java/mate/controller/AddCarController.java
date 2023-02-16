f129886516564e036d963580ce4311e706ae0454
Andrii Varlanovych <andrew_exp@ukr.net>
2023-02-16 14:11:47 +0200

    after clean package

--- src/main/java/mate/controller/AddCarController.java
+++ src/main/java/mate/controller/AddCarController.java
@@ -1,17 +1,17 @@
 package mate.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import mate.lib.Injector;
 import mate.model.Car;
 import mate.model.Manufacturer;
 import mate.service.CarService;
 import mate.service.ManufacturerService;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
 
 @WebServlet(urlPatterns = "/cars/add")
 public class AddCarController extends HttpServlet {
@@ -19,6 +19,7 @@ public class AddCarController extends HttpServlet {
     private final CarService carService = (CarService) injector.getInstance(CarService.class);
     private final ManufacturerService manufacturerService
             = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
