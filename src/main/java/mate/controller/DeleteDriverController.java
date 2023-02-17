55d57303386a655fa94536401a0cc414139f12aa
Denis Pugovkin <den.pugovkin@gmail.com>
2023-02-17 18:30:29 +0200

    1. Created all controllers for creating, getting all and deleting with JSP files. 2. Created redirects after all methods

---
+++ src/main/java/mate/controller/DeleteDriverController.java
@@ -0,0 +1,25 @@
+package mate.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.service.DriverService;
+
+@WebServlet(urlPatterns = "/drivers/delete")
+public class DeleteDriverController extends HttpServlet {
+    private final Injector injector = Injector.getInstance("mate");
+    private DriverService drivererService = (DriverService) injector
+            .getInstance(DriverService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Long id = Long.valueOf(req.getParameter("id"));
+        drivererService.delete(id);
+        resp.sendRedirect("/drivers");
+    }
+}
