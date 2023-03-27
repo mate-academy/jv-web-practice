780d23347fec7db2ae67801cbea7018b5070796c
Vadym Khodorovskyi <vadimhodor@gmail.com>
2023-03-27 14:32:53 +0300

    fixed request changes

--- src/main/java/mate/controller/DeleteDriverController.java
+++ src/main/java/mate/controller/DeleteDriverController.java
@@ -18,6 +18,6 @@ public class DeleteDriverController extends HttpServlet {
             throws ServletException, IOException {
         Long id = Long.valueOf(req.getParameter("id"));
         driverService.delete(id);
-        resp.sendRedirect("/drivers");
+        resp.sendRedirect(req.getContextPath() + "/drivers");
     }
 }
