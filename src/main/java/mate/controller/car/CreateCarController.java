43db0445723e506c3391053f02cb33c10b2fe514
Oleksandr Koval <0993079960a@gmail.com>
2023-04-03 21:20:06 +0300

    added homework

---
+++ src/main/java/mate/controller/car/CreateCarController.java
@@ -0,0 +1,44 @@
+package mate.controller.car;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.model.Car;
+import mate.model.Driver;
+import mate.model.Manufacturer;
+import mate.service.CarService;
+import mate.service.ManufacturerService;
+
+public class CreateCarController extends HttpServlet {
+    private static final Injector injector =
+            Injector.getInstance("mate");
+    private final CarService carService =
+            (CarService) injector.getInstance(CarService.class);
+    private final ManufacturerService manufacturerService =
+            (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/cars/create.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        Long id = Long.valueOf(req.getParameter("manufacturer_id"));
+        Car car = new Car();
+        Manufacturer manufacturer = manufacturerService.get(id);
+        car.setModel(req.getParameter("model"));
+        car.setManufacturer(manufacturer);
+        List<Driver> drivers = new ArrayList<>();
+        car.setDrivers(drivers);
+        carService.create(car);
+        resp.sendRedirect(req.getContextPath() + "/cars");
+    }
+}
