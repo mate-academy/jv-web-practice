d9c48a7dd9ff5000d1c7cf310853e2b3d7363ad8
Nikita <nsalogub96@gmail.com>
2023-04-04 17:19:19 +0300

    refactoring jsp pages

--- src/main/java/mate/controller/car/DisplayAllCarsController.java
+++ src/main/java/mate/controller/car/DisplayAllCarsController.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 import mate.lib.Injector;
 import mate.model.Car;
 import mate.service.CarService;
-import mate.service.ManufacturerService;
 
 public class DisplayAllCarsController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
