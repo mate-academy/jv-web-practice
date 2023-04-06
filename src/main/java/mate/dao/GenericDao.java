12e5c4136ec0028e62252daaebbb03771323ee89
Denys <denchik9026@gmail.com>
2023-04-06 11:16:07 +0300

    implement JSP pages with their corresponding controllers

--- src/main/java/mate/dao/GenericDao.java
+++ src/main/java/mate/dao/GenericDao.java
@@ -4,13 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 public interface GenericDao<T> {
-    T create(T element);
+    T create(T manufacturer);
 
     Optional<T> get(Long id);
 
     List<T> getAll();
 
-    T update(T element);
+    T update(T manufacturer);
 
     boolean delete(Long id);
 }
--- src/main/java/mate/service/GenericService.java
+++ src/main/java/mate/service/GenericService.java
@@ -3,13 +3,13 @@ package mate.service;
 import java.util.List;
 
 public interface GenericService<T> {
-    T create(T element);
+    T create(T manufacturer);
 
     T get(Long id);
 
     List<T> getAll();
 
-    T update(T element);
+    T update(T manufacturer);
 
     boolean delete(Long id);
 }
