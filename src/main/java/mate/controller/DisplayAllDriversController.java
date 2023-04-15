d66e4ebcf4514fa0f2b96fba89a36c2da95e4911
Vitalii Romanyuk <vitaliy37777@gmail.com>
2023-04-15 20:32:51 +0300

    fixed the code by feedback from mentor

--- src/main/java/mate/controller/DisplayAllDriversController.java
+++ src/main/java/mate/controller/DisplayAllDriversController.java
@@ -11,7 +11,7 @@ import mate.lib.Injector;
 import mate.model.Driver;
 import mate.service.DriverService;
 
-@WebServlet(urlPatterns = "/drivers")
+@WebServlet(urlPatterns = "/drivers/all")
 public class DisplayAllDriversController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
     private final DriverService driverService = (DriverService) injector
