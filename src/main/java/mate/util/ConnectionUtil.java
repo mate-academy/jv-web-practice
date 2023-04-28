ddadef51a157e5545e0d9f0452f4539d0470c1f5
Artur Muliukov <arthur.muliukov@gmail.com>
2023-04-28 14:55:06 +0200

    Solved 1.6

--- src/main/java/mate/util/ConnectionUtil.java
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -6,9 +6,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class ConnectionUtil {
-    private static final String URL = "jdbc:mysql://localhost:3306/"
-            + "taxi?autoReconnect=true&useSSL=false&useUnicode=true&useJDBCCompliantTimezoneShift="
-            + "true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/taxi?serverTimezone=UTC";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "arthurcs123";
     private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
