88d39b9308d6c213bbbd4025b02227fd2f038693
Ihor Sydorenko <sidorenko.igorek@gmail.com>
2023-05-23 20:32:47 +0300

    valid solution v.5

--- src/main/java/mate/controller/AddDriverToCarController.java
+++ src/main/java/mate/controller/AddDriverToCarController.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 import mate.lib.Injector;
 import mate.model.Car;
 import mate.model.Driver;
