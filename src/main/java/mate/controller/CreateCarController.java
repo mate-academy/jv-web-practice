9c873c5f486f61574563dee65e5a77548dce9b83
Oleh Pryshchepa <poleja122@gmail.com>
2023-06-07 12:59:17 +0300

    first version

---
+++ src/main/java/mate/controller/CreateCarController.java
@@ -0,0 +1,42 @@
+package mate.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import mate.lib.Injector;
+import mate.model.Car;
+import mate.model.Driver;
+import mate.model.Manufacturer;
+import mate.service.CarService;
+import mate.service.ManufacturerService;
+
+@WebServlet("/cars/create")
+public class CreateCarController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("mate");
+    private final CarService carService = (CarService) injector.getInstance(CarService.class);
+    private final ManufacturerService manufacturerService
+            = (ManufacturerService) injector.getInstance(ManufacturerService.class);
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/car/createCar.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String model = req.getParameter("model");
+        Long manufactureId = Long.valueOf(req.getParameter("manufactureId"));
+        Manufacturer manufacturer = manufacturerService.get(manufactureId);
+        List<Driver> driverList = new ArrayList<>();
+        Car car = new Car(model, manufacturer, driverList);
+        carService.create(car);
+        resp.sendRedirect(req.getContextPath() + "/cars/all");
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/manufacturer/createManufacturer.jsp
@@ -0,0 +1,16 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Create Manufacturer page</title>
+    <%@include file='/WEB-INF/views/common/header.html' %>
+</head>
+<body>
+<h1>Fill fields to create manufacture:</h1>
+<form method="post" action="${pageContext.request.contextPath}/manufacturers/create">
+    Name <input type="text" name="name"><br>
+    Country <input type="text" name="country"><br>
+    <button type="submit">Confirm</button>
+</form>
+</body>
+</html>
