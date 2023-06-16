76a43f695ed6997b03d70f78c6b7b61a2d5ed507
Vladyslav Krutyus <kijdron@gmail.com>
2023-06-16 11:30:38 +0300

    ConnectionUtil updated

--- src/main/java/mate/util/ConnectionUtil.java
+++ src/main/java/mate/util/ConnectionUtil.java
@@ -9,7 +9,7 @@ public class ConnectionUtil {
     private static final String MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "p1y0LD!F%QNl3L9oEl4jh!$t";
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/taxi_db";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/taxi_db?serverTimezone=UTC";
 
     static {
         try {
