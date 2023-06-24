7944e71e68abf37a4520313042b1a886180af5ab
Mykola Panasiuk <mikola.panasyuk.01@gmail.com>
2023-06-25 01:26:25 +0300

    fixed url naming

--- src/main/java/mate/controller/AddDriverToCarController.java
+++ src/main/java/mate/controller/AddDriverToCarController.java
@@ -13,7 +13,7 @@ import mate.model.Driver;
 import mate.service.CarService;
 import mate.service.DriverService;
 
-@WebServlet("/cars/add_driver")
+@WebServlet("/cars/drivers/add")
 public class AddDriverToCarController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
     private final CarService carService = (CarService) injector.getInstance(CarService.class);
