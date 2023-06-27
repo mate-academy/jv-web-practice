6f63a85177bf35bce63c565a030849da50275bbd
Victor Ruban <edmont373@gmail.com>
2023-06-27 16:31:57 +0300

    Made changes after mentor review

--- src/main/java/mate/controller/car/GetAllCarController.java
+++ src/main/java/mate/controller/car/GetAllCarController.java
@@ -11,7 +11,7 @@ import mate.lib.Injector;
 import mate.model.Car;
 import mate.service.CarService;
 
-@WebServlet("/cars")
+@WebServlet("/cars/all")
 public class GetAllCarController extends HttpServlet {
     private static final Injector injector = Injector.getInstance("mate");
     private final CarService carService = (CarService)
