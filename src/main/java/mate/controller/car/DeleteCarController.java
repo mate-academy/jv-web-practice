4b7341b236af4f4da1014a6b621066840a20adf0
Vershynin Kostia <bigvini@gmail.com>
2023-08-08 00:09:50 +0300

    realization jv-web-practice

--- src/main/java/mate/controller/car/DeleteCarController.java
+++ src/main/java/mate/controller/car/DeleteCarController.java
@@ -2,8 +2,8 @@ package mate.controller.car;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import mate.lib.Injector;
